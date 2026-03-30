09348cf0f3e657a4fe2691982c59ac4809b22e6f
Tanjil Rafi <tanjilsarker77@gmail.com>
2026-03-30 11:55:57 -0600

    Delete authors directory

--- authors/api_views/__init__.py
+++
@@ -1,7 +0,0 @@
-from .author_api_views import *
-from .comment_api_views import *
-from .entry_api_views import *
-from .follow_api_views import *
-from .inbox_api_views import *
-from .like_api_views import *
-from .stream_api_views import *
