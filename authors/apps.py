f6f05815b995b9445cf46afb0679c3e680cd66e3
Tanjil Rafi <tanjilsarker77@gmail.com>
2026-03-29 23:18:49 -0600

    Delete authors directory

--- authors/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthorsConfig(AppConfig):
-    name = "authors"
-
-    def ready(self):
-        # Import signals to ensure they’re registered
-        import authors.signals
